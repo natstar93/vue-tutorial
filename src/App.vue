5925b0db5657bb17662cf40d1cfc6f6935b67a8a
Natalie Akam <natalie.akam@gmail.com>
2020-02-18 22:06:35 +0000

    Set up basic employee table and form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-table :employees="employees" />
+    <employee-form @add:employee="addEmployee" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 0,
+          name: "Nat",
+          email: "nat@piedpiper.com"
+        },
+        {
+          id: 1,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const id = this.employees.length;
+      this.employees = [...this.employees, { ...employee, id }];
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
